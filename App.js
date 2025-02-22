806bff17d50552c4ce860e5497d2cac72cacbf3c
Samuel Niang <samuel.niang@cern.ch>
2025-02-22 17:31:42 +0100

    display some cards

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Platform, ScrollView } from 'react-native';
+import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+
+  const charmanderData = {
+    name: 'Charmander',
+    type: 'Fire',
+    hp: 39,
+    moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
+    weaknesses: ['Water', 'Rock'],
+    image: require('./assets/charmander.png'),
+  }
+
+  const pikachuData = {
+    name: 'Pikachu',
+    type: 'Electric',
+    hp: 35,
+    moves: ['Thunder Shock', 'Growl', 'Tail Whip', 'Quick Attack'],
+    weaknesses: ['Ground'],
+    image: require('./assets/pikachu.png'),
+  }
+
+  const sandslashData = {
+    name: 'Sandslash',
+    type: 'Ground',
+    hp: 75,
+    moves: ['Sand Attack', 'Slash', 'Poison Sting', 'Swift'],
+    weaknesses: ['Water', 'Grass', 'Ice'],
+    image: require('./assets/sandslash.png')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <PokemonCard {...charmanderData}/>
+      <PokemonCard {...pikachuData}/>
+      <PokemonCard {...sandslashData}/>
+      </ScrollView> 
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
